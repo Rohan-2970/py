98d23739a83b3822b68e7c944e6c428688a49f3a
Rohan-2970 <rohansaxena0905@gmail.com>
2026-01-16 23:53:55 +0530

    comment

--- 2_Variables and datatype/prblm2.py
+++ 2_Variables and datatype/prblm2.py
@@ -1,3 +1,4 @@
 a=5
 b=3
 print("py program to find remainder when a is divided by b : ",a%b)
+# to find the remainder
--- p.py
+++ p.py
@@ -1,4 +1,3 @@
 a=int(input("Enter the number: "))
 b=int(input("Enter the number: "))
-print("The square of a is: ", a*a)
-print("The square of a is: ",a**2)
+print("to find the remainder")
