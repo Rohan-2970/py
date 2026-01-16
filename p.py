50e165547c7e0b3f396bcf812a788836eaa6a7ed
Rohan-2970 <rohansaxena0905@gmail.com>
2026-01-16 23:44:21 +0530

    practice

--- p.py
+++ p.py
@@ -1,12 +1,2 @@
-a=1
-b=2
-c="3.65"
-j=float(c)
-d=False
-e= None
-t=type(j)
-print(t)
-g=int(input("Enter the number 1: "))
-h=int(input("Enter the second number: "))
-print("Number a is : ",g)
-print("Number b is : ",g+h)
+a = input("Enter the input: ")
+print(type(a))
