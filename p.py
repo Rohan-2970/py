56cd505a75bbef2f958792c077c82c46da25b535
Rohan-2970 <rohansaxena0905@gmail.com>
2026-01-19 18:53:38 +0530

    im provising

--- p.py
+++ p.py
@@ -1,7 +1,15 @@
-def avg():
-    a=int(input())
-    b=int(input())
-    c=int(input())
-    average = (a+b+c)/3
-    print(average)
-avg()
+# def avg():
+#     a=int(input())
+#     b=int(input())
+#     c=int(input())
+#     average = (a+b+c)/3
+#     print(average)
+# avg()
+
+def goodDay(name, ending):
+    print("Good Day, " + name)
+    print(ending)
+    return "ok"
+
+goodDay("Harry", "Thank you") 
+# print(a)
